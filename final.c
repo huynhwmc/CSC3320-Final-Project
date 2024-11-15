2cd3f7775800f9cd7f24d8b569ecf4cddf63cbe1
huynhwmc <156921205+huynhwmc@users.noreply.github.com>
2024-11-15 14:22:43 -0500

    first commit

---
+++ final.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    // Banking system
+    // Manage each user's accounts, balances, and withdrawals and deposits
+
+    // OR chat system
+
+    // This pointer will hold the
+    // base address of the block created
+    int *ptr;
+    int n, i;
+
+    // Get the number of elements for the array
+    printf("Enter number of elements:");
+    scanf("%d", &n);
+    printf("Entered number of elements: %d\n", n);
+
+    // Dynamically allocate memory using malloc()
+    ptr = (int *)malloc(n * sizeof(int));
+
+    // Check if the memory has been successfully
+    // allocated by malloc or not
+    if (ptr == NULL)
+    {
+        printf("Memory not allocated.\n");
+        exit(0);
+    }
+    else
+    {
+        // Memory has been successfully allocated
+        printf("Memory successfully allocated using malloc.\n");
+
+        // Get the elements of the array
+        for (i = 0; i < n; ++i)
+        {
+            ptr[i] = i + 1;
+        }
+
+        // Print the elements of the array
+        printf("The elements of the array are: ");
+        for (i = 0; i < n; ++i)
+        {
+            printf("%d, ", ptr[i]);
+        }
+    }
+    
+    // free the allocated memory
+    free(ptr);
+    printf("\nAllocated memory successfully freed");
+
+    // Set 'ptr' NULL to avoid the accidental use
+    ptr = NULL;
+
+    return 0;
+}
