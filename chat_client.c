1e73bc860c30f06f354565a54cdb639dec60050e
huynhwmc <156921205+huynhwmc@users.noreply.github.com>
2024-11-15 20:08:02 -0500

    begin the chat application code using IPC & dynamic memory

---
+++ chat_client.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Chat client */
+int main(int argc, char *argv[])
+{
+    // User enters a message
+
+    // Allocate dynamic memory for the message
+
+    // Connect to the chat server using shared memory
+
+    // Use mutexes to ensure no other process is accessing or updating that memory
+
+    // After the message has been successfully sent, free the allocated memory
+    return 0;
+}
---
+++ chat_server.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Chat system server */
+int main(int argc, char *argv[])
+{
+    printf("\nWelcome to the OS Chat Server");
+    printf("\nType '.exit' to exit.");
+    // Create threads waiting for clients to connect
+
+    // TODO Dynamic memory test, for messages
+    // This pointer will hold the
+    // base address of the block created
+    int *ptr;
+    int n, i;
+
+    // Get the number of elements for the array
+    printf("Enter number of elements:");
+    scanf("%d", &n);
+    printf("Entered number of elements: %d\n", n);
+
+    // Dynamically allocate memory using malloc()
+    ptr = (int *)malloc(n * sizeof(int));
+
+    // Check if the memory has been successfully
+    // allocated by malloc or not
+    if (ptr == NULL)
+    {
+        printf("Memory not allocated.\n");
+        exit(0);
+    }
+    else
+    {
+        // Memory has been successfully allocated
+        printf("Memory successfully allocated using malloc.\n");
+
+        // Get the elements of the array
+        for (i = 0; i < n; ++i)
+        {
+            ptr[i] = i + 1;
+        }
+
+        // Print the elements of the array
+        printf("The elements of the array are: ");
+        for (i = 0; i < n; ++i)
+        {
+            printf("%d, ", ptr[i]);
+        }
+    }
+    
+    // free the allocated memory
+    free(ptr);
+    printf("\nAllocated memory successfully freed");
+
+    // Set 'ptr' NULL to avoid the accidental use
+    ptr = NULL;
+
+    return 0;
+}
