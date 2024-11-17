f39a12d51b7ffc85d6496c4c003ff2bc4a697545
huynhwmc <156921205+huynhwmc@users.noreply.github.com>
2024-11-17 16:46:21 -0500

    client's message input

--- chat_client.c
+++ chat_client.c
@@ -1,10 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/shm.h>
+#include <string.h>
+#include <sys/wait.h>
 
 /* Chat client */
 int main(int argc, char *argv[])
 {
+    char **strings = NULL; // Holds the message in dynamic memory
+    int i = 0;
+    char str[200], ch;
+
     // User enters a message
+    printf("\nEnter a message: ");
+
+    printf("\n Enter Your message (press @ to terminate) \n");
+    while ((ch = fgetc(stdin)) != '@')
+    {
+        str[i++] = ch;
+    }
+    str[i] = '\0';
+
+    int shmid;
+    void *shared_memory;
+    char buff[100];
+
+    // Create shared memory
+    shmid = shmget((key_t)2345, 1024, 0666 | IPC_CREAT);
+    if (shmid == -1)
+    {
+        perror("shmget");
+        return 1;
+    }
+
+    shared_memory = shmat(shmid, NULL, 0);
+    if (shared_memory == (void *)-1)
+    {
+        perror("shmat");
+        return 1;
+    }
 
     // Allocate dynamic memory for the message
 
