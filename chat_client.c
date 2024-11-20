b394844081515356114328cedc92379a759c06c5
huynhwmc <156921205+huynhwmc@users.noreply.github.com>
2024-11-20 10:21:20 -0500

    client mutex, server comments

--- chat_client.c
+++ chat_client.c
@@ -3,44 +3,57 @@
 #include <sys/shm.h>
 #include <string.h>
 #include <sys/wait.h>
+#include <pthread.h>
 
-#define MAX_LIMIT 200
+#define MAX_LIMIT 200 // Maximum buffer/character limit for messages
+pthread_mutex_t mutex;
+pthread_cond_t full;
+pthread_cond_t empty;
 
 /* Chat client */
 int main(int argc, char *argv[])
 {
     char **strings = NULL; // Holds the message in dynamic memory
-    int i = 0;
     char str[MAX_LIMIT], ch;
     int shmid; // Shared memory's ID
     void *shared_memory;
     char buff[100];
-
+    // Define mutual exclusion
+    pthread_mutex_init(&mutex, NULL);
+    pthread_cond_init(&full, NULL);
+    pthread_cond_init(&empty, NULL);
+    
     // User enters a message
     printf("\nEnter a message: ");
     fgets(str, MAX_LIMIT, stdin);
-
-        // Create shared memory
+    
+    // Allocate dynamic memory for the message
+    // TODO: Create dynamic memory for the message using malloc()
+    
+    // Create shared memory
     shmid = shmget((key_t)2345, 1024, 0666 | IPC_CREAT);
     if (shmid == -1)
     {
         perror("shmget");
         return 1;
     }
-
+    
     shared_memory = shmat(shmid, NULL, 0);
     if (shared_memory == (void *)-1)
     {
         perror("shmat");
         return 1;
     }
-
-    // Allocate dynamic memory for the message
-
+    
+    
     // Connect to the chat server using shared memory
-
+    // TODO: Connect to the chat server using shared memory
+    
     // Use mutexes to ensure no other process is accessing or updating that memory
-
+    
     // After the message has been successfully sent, free the allocated memory
+    pthread_mutex_destroy(&mutex);
+    pthread_cond_destroy(&full);
+    pthread_cond_destroy(&empty);
     return 0;
 }
--- chat_server.c
+++ chat_server.c
@@ -9,7 +9,7 @@ pthread_cond_t empty;
 char message[200];
 
 void *receive_msg(void *threadarg) {
-    // TODO Access shared memory and read the message
+    // TODO: Access shared memory and read the message
     pthread_exit(NULL);
 }
 
@@ -20,33 +20,35 @@ int main(int argc, char *argv[])
     
     // Create threads waiting for clients to connect
     pthread_t serverThread;
+    // Create the mutex
     pthread_mutex_init(&mutex, NULL);
     pthread_cond_init(&full, NULL);
     pthread_cond_init(&empty, NULL);
-
+    
     char input[20];
     char word[] = ".exit";
     char *result;
-
     
+    // Start receiving messages from clients
     pthread_create(&serverThread, NULL, receive_msg, NULL);
     pthread_join(serverThread, NULL);
+    
+    // Server CLI
     printf("\nNow waiting for messages.");
-    printf("\nType '.exit' to exit.");
-    while (1) // While true
+    printf("\nType '.exit' to exit.\n");
+    
+    while (1) // While true, waiting for the "exit" message
     {
-        // Wait for a client to connect and receive a message
-        //
         fgets(input, 20, stdin);
         // Using strstr() to find the word
         result = strstr(input, word);
         if (result != NULL)
         {
-            printf("Exiting...");
-            break;
+            printf("\nExiting...\n");
+            break; // Terminate from the infinite loop and execute the code below
         }
     }
-
+    
     pthread_mutex_destroy(&mutex);
     pthread_cond_destroy(&full);
     pthread_cond_destroy(&empty);
