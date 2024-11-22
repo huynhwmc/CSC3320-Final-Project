5f2efd8fc368daa62a6b1922dbc5d2e0cf24a14d
huynhwmc <156921205+huynhwmc@users.noreply.github.com>
2024-11-22 12:32:04 -0500

    server's shared memory stub

--- chat_server.c
+++ chat_server.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <sys/shm.h>
 #include <string.h>
+#include <sys/wait.h>
 
 pthread_mutex_t mutex;
 pthread_cond_t full;
 pthread_cond_t empty;
 char message[200];
+int shmid; // Shared memory's ID
+void *shared_memory;
+char buff[100];
 
 void *receive_msg(void *threadarg) {
     // TODO: Access shared memory and read the message
+    
     pthread_exit(NULL);
 }
 
@@ -30,6 +36,20 @@ int main(int argc, char *argv[])
     char *result;
 
     // Start receiving messages from clients
+    // Create shared memory
+        shmid = shmget((key_t)2345, 1024, 0666 | IPC_CREAT);
+        if (shmid == -1)
+        {
+            perror("shmget");
+            return 1;
+        }
+        
+        shared_memory = shmat(shmid, NULL, 0);
+        if (shared_memory == (void *)-1)
+        {
+            perror("shmat");
+            return 1;
+        }
     pthread_create(&serverThread, NULL, receive_msg, NULL);
     pthread_join(serverThread, NULL);
 
